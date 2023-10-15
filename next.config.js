58d60d55a9a6cbca81519c3c49055209e280532c
Tosin Amuda <tosinamuda@gmail.com>
2023-10-15 14:32:39 +0100

    chore(redirect): add old redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  }
+  },
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://tosinamuda.com' : undefined,
 }
 
 module.exports = nextConfig
